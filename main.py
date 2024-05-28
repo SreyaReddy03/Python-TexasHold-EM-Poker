e92cc8eec29a312f661462103e0f61bfb7646d18
SreyaReddy03 <118820827+SreyaReddy03@users.noreply.github.com>
2024-05-28 11:23:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from poker.card import Card
+from poker.deck import Deck
+from poker.game_round import GameRound
+from poker.hand import Hand
+from poker.player import Player
+
+deck = Deck()
+cards= Card.create_standard_52_cards()
+deck.add_cards(cards)
+
+hand1 = Hand()
+hand2 = Hand()
+
+player1 = Player(name ="Boris", hand = hand1)
+player2 = Player(name ="Bobby", hand = hand2)
+players =[player1, player2]
+
+game_round = GameRound(deck = deck, players = players)
+game_round.play()
+
+
+#from main import deck,cards,game_round,hand1,hand2,player1,player2
